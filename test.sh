14a6cdb4eff68da4bb28be85892a8bec38a53ae1
Leo Blondel <xqua@free.fr>
2018-10-23 18:57:31 -0400

    fixed test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nvidia-docker run -v $(pwd)/results:/usr/src/app/TGMM/TGMMruns -it e05d512e9460 0 30 4
+nvidia-docker run -v $(pwd)/data:/usr/src/app/TGMM/data -v $(pwd)/results:/usr/src/app/TGMM/TGMMruns -it e05d512e9460 0 30 4
